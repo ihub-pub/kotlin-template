1c3e24e97f8561dda286d290091e8892c11b4fb3
henry <henry.git@outlook.com>
2023-04-16 21:55:14 +0800

    build(github): 升级gradle至8.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-    id("pub.ihub.plugin.ihub-java")
+    id("pub.ihub.plugin.ihub-kotlin")
     id("pub.ihub.plugin.ihub-test")
     id("pub.ihub.plugin.ihub-verification")
     id("pub.ihub.plugin.ihub-git-hooks")
-    kotlin("jvm") version "1.8.0"
 }
 
 iHubGitHooks {
-    hooks = mapOf("pre-commit" to "./gradlew build", "commit-msg" to "./gradlew commitCheck")
+    hooks.set(mapOf(
+        "pre-commit" to "./gradlew build",
+        "commit-msg" to "./gradlew commitCheck"
+    ))
 }
