95cae7eddd03bc3a67da372374dfd698aff015c0
Henry <henry.git@outlook.com>
2025-06-19 22:14:24 +0800

    build: 更新 Kotlin 版本并添加 BOM 导入
    - 添加 Spring Boot 尚未兼容 Kotlin 2.0 的 TODO 注释
    - 在 iHubBom 中导入 Kotlin BOM，版本为2.1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,13 @@ plugins {
     id("pub.ihub.plugin.ihub-git-hooks")
 }
 
+// TODO Spring Boot 还未兼容kotlin2.0
+iHubBom {
+    importBoms {
+        group("org.jetbrains.kotlin").module("kotlin-bom").version("2.1.21")
+    }
+}
+
 iHubGitHooks {
     hooks.set(mapOf(
         "pre-commit" to "./gradlew build",
